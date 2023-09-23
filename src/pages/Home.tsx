9a1490cd8852daeb07b72665e8030b7f5ab575d1
danielerat <davidodo2015@gmail.com>
2023-09-23 13:56:22 +0200

    UI Design Part

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,23 @@
+import Input from "../components/shared/Input";
+
 const Home = () => {
-  return <></>;
+  return (
+    <>
+      <div className="md:flex items-center p-10">
+        <div className="basis-1/2 p-5 bg-gray-100 rounded-lg">
+          <div className="flex flex-col gap-10">
+            <Input name="Description" type="text"></Input>
+            <Input name="Amount" type="text"></Input>
+          </div>
+        </div>
+        <div className="basis-1/2 p-5">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat vel
+          ipsam iure sit fugit fugiat, totam et odio, debitis facilis rem ipsum
+          ab necessitatibus sequi, soluta assumenda impedit dicta? Ullam.
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Home;
