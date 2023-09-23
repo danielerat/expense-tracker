e2f43b444c08e08510447f251b1aa4ed9cf2e2c4
danielerat <davidodo2015@gmail.com>
2023-09-23 17:07:05 +0200

    ESLINT & TS Error ignore on input with error prop

--- src/components/shared/Input.tsx
+++ src/components/shared/Input.tsx
@@ -3,17 +3,27 @@ import { FieldValues, UseFormRegister } from "react-hook-form";
 interface Props {
   children?: string;
   name: string;
+  fieldError: string | undefined;
   type: "text" | "password" | "number" | "email";
   refRegister: UseFormRegister<FieldValues>;
 }
-const Input = ({ name, type, refRegister, children: label = name }: Props) => {
+const Input = ({
+  name,
+  type,
+  refRegister,
+  fieldError,
+  children: label = name,
+}: Props) => {
   return (
     <div>
       <label
         htmlFor={name}
         className="relative block rounded-md border font-bold border-gray-400 shadow-sm focus-within:border-blue-600 focus-within:ring-1 focus-within:ring-blue-600">
         <input
-          {...refRegister(name)}
+          {...refRegister(
+            name,
+            type === "number" ? { valueAsNumber: true } : {}
+          )}
           type={type}
           id={name}
           className="peer border-none bg-transparent placeholder-transparent focus:border-transparent p-3 focus:outline-none focus:ring-0"
@@ -24,6 +34,11 @@ const Input = ({ name, type, refRegister, children: label = name }: Props) => {
           {label}
         </span>
       </label>
+      {fieldError && (
+        <p className="text-xs text-yellow-500 font-medium">
+          {String(fieldError)}
+        </p>
+      )}
     </div>
   );
 };
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,27 +1,38 @@
-import { useRef, useState } from "react";
+import { useState } from "react";
 import ExpenseList from "../components/ExpenseList";
 import Button from "../components/shared/Button";
 import Input from "../components/shared/Input";
 import SelectInput from "../components/shared/SelectInput";
 import { useForm, FieldValues } from "react-hook-form";
-const Home = () => {
-  const description = useRef(null);
-  const amount = useRef(null);
-  const category = useRef(null);
-  const { register, handleSubmit } = useForm();
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
 
-  const [expenses, setExpenses] = useState([
+const schema = z.object({
+  description: z.string().min(3),
+  amount: z.number().min(1),
+  category: z.string(),
+});
+// type FormData = z.infer<typeof schema>;
+
+const Home = () => {
+  const [expenses] = useState([
     { id: 1, description: "Milk", amount: 1300, category: "Grocery" },
   ]);
 
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isValid, isSubmitted },
+  } = useForm({
+    resolver: zodResolver(schema),
+  });
+
   function onSubmit(data: FieldValues) {
     console.log(data);
-    console.log(amount?.current);
-    console.log(description?.current);
-    console.log(category?.current);
-    setExpenses((current) => {
-      return [...current];
-    });
+    console.log();
+
+    console.log(isValid);
+    console.log(isSubmitted);
   }
   return (
     <>
@@ -29,10 +40,21 @@ const Home = () => {
         <div className="basis-1/2 p-5 bg-gray-100 rounded-lg">
           <form onSubmit={handleSubmit(onSubmit)}>
             <div className="flex flex-col gap-10">
-              <Input refRegister={register} name="description" type="text">
+              <Input
+                refRegister={register}
+                name="description"
+                type="text"
+                // @ts-ignore
+                fieldError={errors?.description?.message}>
                 Description
               </Input>
-              <Input refRegister={register} name="amount" type="number">
+
+              <Input
+                refRegister={register}
+                name="amount"
+                type="number"
+                // @ts-ignore
+                fieldError={errors?.amount?.message}>
                 Amount
               </Input>
               <SelectInput refRegister={register} name="type">
@@ -42,7 +64,7 @@ const Home = () => {
             </div>
           </form>
         </div>
-        <div className="basis-1/2 p-5">
+        <div className="bg-gray-100 basis-1/2 p-5">
           <ExpenseList expenses={expenses}></ExpenseList>
         </div>
       </div>
